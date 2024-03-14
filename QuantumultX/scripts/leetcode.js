ba0b363a5c265b13e0566afc475078ac0be38d92
hedroid <heweijie.cn@gmail.com>
2024-03-14 11:22:42 +0800

    Create leetcode.js

---
+++ QuantumultX/scripts/leetcode.js
@@ -0,0 +1,34 @@
+/*************************************
+
+ 项目名称：leetcode
+ 脚本作者：hedroid
+ 使用声明：⚠️仅供参考，🈲转载与售卖！
+
+ **************************************
+
+ [rewrite_local]
+
+ ^https:\/\/leetcode\.cn\/graphql\/(app)?$ url script-response-body https://raw.githubusercontent.com/hedroid/jailbreak/main/QuantumultX/scripts/leetcode.js
+
+ [mitm]
+
+ hostname = leetcode.cn
+ 
+ *************************************/
+
+let leetcode = JSON.parse($response.body);
+const premiumExpiredAt=4084012799000
+const method = $request.headers["X-APOLLO-OPERATION-NAME"];
+
+if (method === "UserPremiumInfo") {
+    leetcode.data.userStatus.isPremium = true;
+    leetcode.data.userStatus.premiumExpiredAt = premiumExpiredAt;
+} else if (method === "restoreAppleReceiptInput") {
+    leetcode.data.restoreAppleReceipt.ok = true;
+    leetcode.data.restoreAppleReceipt.message = "恢复成功";
+} else if (method === "UserStatus") {
+    leetcode.data.isPremium = true;
+    leetcode.data.isSuperuser = true;
+    leetcode.data.premiumExpiredAt = premiumExpiredAt;
+}
+$done({body : JSON.stringify(leetcode)});
