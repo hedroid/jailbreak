08e32fe6640ff84299b1844f63463259ca794f59
hedroid <heweijie.cn@gmail.com>
2024-03-15 22:00:24 +0800

    Update RevenueCat.js

--- QuantumultX/scripts/RevenueCat.js
+++ QuantumultX/scripts/RevenueCat.js
@@ -8,8 +8,8 @@
  **************************************
 
  [rewrite_local]
- ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
- ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
+ ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/hedroid/jailbreak/main/QuantumultX/scripts/RevenueCat.js
+ ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/hedroid/jailbreak/main/QuantumultX/scripts/RevenueCat.js
  ^https?:\/\/app-measurement\.com\/config\/app url reject
  ^https?:\/\/firestore\.googleapis\.com url reject
 
