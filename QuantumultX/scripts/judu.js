1879113cfea35bbec4b138b58d233064987a09d0
hedroid <heweijie.cn@gmail.com>
2024-03-26 09:06:51 +0800

    Create judu.js

---
+++ QuantumultX/scripts/judu.js
@@ -0,0 +1,18 @@
+[rewrite_local]
+# > 句读 vip
+^^https?:\/\/judouapp.com\/api\/v2\/mine\/profile url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/judu.js
+
+[mitm] 
+hostname = judouapp.com
+*
+*
+*/
+var body = $response.body; 
+let obj = JSON.parse($response.body);
+obj.is_member = true ;
+obj.is_admin = true ;
+//obj.is_year_member = true ;
+obj.nickname = "hedroid" ;
+obj.member_expired_at = 32493834549 ;
+obj.is_bind_phone = true ;
+$done({body: JSON.stringify(obj)});
