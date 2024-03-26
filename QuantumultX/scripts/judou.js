e721f12901b91950a14cce52e600094afb3ad02b
hedroid <heweijie.cn@gmail.com>
2024-03-26 09:46:50 +0800

    Update judou.js

--- QuantumultX/scripts/judou.js
+++ QuantumultX/scripts/judou.js
@@ -1,17 +1,15 @@
 [rewrite_local]
 # > 句读
-^https?:\/\/judouapp.com\/api\/v2\/mine\/profile url script-response-body https://raw.githubusercontent.com/hedroid/jailbreak/main/QuantumultX/scripts/judou.js
+^^https?:\/\/judouapp.com\/api\/v2\/mine\/profile url script-response-body https://raw.githubusercontent.com/hedroid/jailbreak/main/QuantumultX/scripts/judou.js
 
 [mitm] 
 hostname = judouapp.com
 *
 *
 */
-var body = $response.body; 
 let obj = JSON.parse($response.body);
 obj.is_member = true ;
 obj.is_admin = true ;
 obj.is_year_member = true ;
 obj.member_expired_at = 32493834549 ;
-obj.is_bind_phone = true ;
 $done({body: JSON.stringify(obj)});
