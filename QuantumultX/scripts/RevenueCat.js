75621af374cd754059856c59dfac755140b44ce2
hedroid <heweijie.cn@gmail.com>
2025-02-21 10:28:29 +0800

    Update RevenueCat.js

--- QuantumultX/scripts/RevenueCat.js
+++ QuantumultX/scripts/RevenueCat.js
@@ -27,7 +27,6 @@ const bundle_id = headers['X-Client-Bundle-ID'] || headers['x-client-bundle-id']
 
 const apps = {
     'Noto%20%E7%AC%94%E8%AE%B0': {id: 'com.lkzhao.editor.pro.ios.monthly', name: 'pro', expire: 1},
-    'Reader': {id: 'reader.lifetime.pro', name: 'pro', expire: 1}, //PureLibro
     'One4WallSwiftUI': {id: 'lifetime_key', name: 'lifetime', expire: 0}, //One4Wall
     'PDF%20Viewer': {id: 'com.pspdfkit.viewer.sub.pro.yearly', name: 'sub.pro', expire: 1},  //PDF Viewerr
     'Paper': {id: 'com.fiftythree.paper.credit', name: 'pro', expire: 0},  //Paper素描
